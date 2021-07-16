f98a4a6063e6738b7dd66748d294be30040acb61
Jomari Iringan <jomari.iringan@olegroups.com>
2021-07-16 14:18:38 +0800

    modify nav on logo

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -26,7 +26,7 @@ export default function(props){
             variant="dark">
             <Container>
                 <Navbar.Brand as={Link}
-                        to="/#">@jomsthegreat</Navbar.Brand>
+                        to="/#">@jooomsthegreat</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto" />
